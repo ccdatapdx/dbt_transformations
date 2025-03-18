2413cc521e773e87b4a1659acf485b4fda1bcecb
Casey Cushing <caseycushing@gmail.com>
2025-03-18 04:08:35 +0000

    weekly reporting tables v1

---
+++ models/techmeme_nlp/reporting/weekly_reporting_pubs.sql
@@ -0,0 +1,14 @@
+with source as (
+
+    select 
+    pub,
+    headline,
+    row_number() over (partition by headline) as row_num
+    from {{ref("staging_weekly_reporting")}}
+)
+
+select pub, count(pub) as pub_count
+from source
+where row_num = 1
+group by pub
+order by pub_count desc
